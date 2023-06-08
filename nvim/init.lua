ef985485806ee12138088b4f06600fb549fb005e
BerryHeyy <berkaykaplanbk@hotmail.com>
2023-06-08 18:19:21 +0200

    Updated dotfiles, primarily desktop

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,32 +6,39 @@ local Plug = vim.fn["plug#"]
 
 vim.call("plug#begin")
 
---Plug 'andweeb/presence.nvim'
+Plug 'andweeb/presence.nvim'
 --Plug 'preservim/nerdtree'
-Plug 'gruvbox-community/gruvbox'
---Plug ('nvim-treesitter/nvim-treesitter', {['do'] = 'TSUpdate'})
+--Plug 'gruvbox-community/gruvbox'
+Plug 'ellisonleao/gruvbox.nvim'
+Plug ('nvim-treesitter/nvim-treesitter', {['do'] = 'TSUpdate'})
 --Plug 'tpope/vim-fugitive'
 
 vim.call("plug#end")
 
 -- General settings
 
+vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
 
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.colorcolumn = "120"
 
 vim.keymap.set("i", "jk", "<ESC>")
+
+vim.keymap.set("n", "<C-k>", ":b#<Cr>")
 
 --vim.keymap.set("n", "<F7>", ":tabp<CR>")
 --vim.keymap.set("n", "<F8>", ":tabn<CR>")
 
 -- Discord Presence Settings
 
---vim.g.presence_show_time = 0
---vim.g.presence_main_image = "file"
+vim.g.presence_show_time = 0
+vim.g.presence_main_image = "file"
 
 -- NERDTree Settings
 
@@ -39,23 +46,23 @@ vim.keymap.set("i", "jk", "<ESC>")
 
 -- TreeSitter Settings
 
---local status_ok, configs = pcall(require, "nvim-treesitter.configs")
---
---if not status_ok then
---	return
---end
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 
---configs.setup(
---{
---	ensure_installed = {"cpp", "lua", "c", "cmake" },
---	sync_install = false,
---	ignore_install = {""},
---	
---	highlight = {
---		enable = true,
---		disable = {""},
---		additional_vim_regex_highlighting = true
---	}
---}
---);
+if not status_ok then
+	return
+end
+
+configs.setup(
+{
+	ensure_installed = {"cpp", "lua", "c", "cmake", "make", "markdown_inline"},
+	sync_install = false,
+	ignore_install = {""},
+	
+	highlight = {
+		enable = true,
+		disable = {""},
+		additional_vim_regex_highlighting = true
+	}
+}
+);
 

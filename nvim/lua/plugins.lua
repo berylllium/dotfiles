639eb3be537f7c3ce5f4187e6833600fbcd8d95c
berylllium <berkaykaplanbk@hotmail.com>
2023-09-05 21:54:11 +0200

    Added neorg plugin, disabled ufo.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -211,34 +211,54 @@ local plugins = {
 		'theHamsta/nvim-dap-virtual-text',
 		dependencies = { "mfussenegger/nvim-dap" },
 		config = function()
-			require("nvim-dap-virtual-text").setup()
+			require("nvim-dap-virtual-text").setup({})
 		end
 	},
+--	{
+--		"kevinhwang91/nvim-ufo",
+--		dependencies = { "kevinhwang91/promise-async", 'nvim-treesitter/nvim-treesitter' },
+--		config = function()
+--			vim.o.foldcolumn = '1' -- '0' is not bad
+--			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+--			vim.o.foldlevelstart = 99
+--			vim.o.foldenable = true
+--
+--			vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+--			vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+--
+----			local capabilities = vim.lsp.protocol.make_client_capabilities()
+----			capabilities.textDocument.foldingRange = {
+----			    dynamicRegistration = false,
+----			    lineFoldingOnly = true
+----			}
+----			local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+----			for _, ls in ipairs(language_servers) do
+----			    require('lspconfig')[ls].setup({
+----			        capabilities = capabilities
+----			        -- you can add other fields for setting up lsp server in this table
+----			    })
+----			end
+--			require('ufo').setup()
+--		end
+--	},
 	{
-		"kevinhwang91/nvim-ufo",
-		dependencies = { "kevinhwang91/promise-async", 'nvim-treesitter/nvim-treesitter' },
+		"nvim-neorg/neorg",
+		build = ":Neorg sync-parsers",
+		dependencies = { 'nvim-treesitter/nvim-treesitter' },
 		config = function()
-			vim.o.foldcolumn = '1' -- '0' is not bad
-			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-			vim.o.foldlevelstart = 99
-			vim.o.foldenable = true
-
-			vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-			vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-
---			local capabilities = vim.lsp.protocol.make_client_capabilities()
---			capabilities.textDocument.foldingRange = {
---			    dynamicRegistration = false,
---			    lineFoldingOnly = true
---			}
---			local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
---			for _, ls in ipairs(language_servers) do
---			    require('lspconfig')[ls].setup({
---			        capabilities = capabilities
---			        -- you can add other fields for setting up lsp server in this table
---			    })
---			end
-			require('ufo').setup()
+			require("neorg").setup {
+				load = {
+					["core.defaults"] = {}, -- Loads default behaviour
+					["core.concealer"] = {}, -- Adds pretty icons to your documents
+					["core.dirman"] = { -- Manages Neorg workspaces
+						config = {
+							workspaces = {
+								notes = "~/documents/notes",
+							},
+						},
+					},
+				}
+			}
 		end
 	}
 }

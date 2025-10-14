0077a6b37fc498617c23f507d4d192859c95a4a6
Berkay Kaplan <berkaykaplan@pm.me>
2025-10-14 22:36:34 +0200

    Doom emacs

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -3,6 +3,9 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+;; True in buffers created from terminal command "magit".
+(setq berry/magit-terminal-buffer-p nil)
+
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
@@ -21,7 +24,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Fira Code" :size 13 :weight 'semi-light))
+;; (setq doom-font (font-spec :family "Fira Code" :size 13 :weight 'semi-light))
+(setq doom-font (font-spec :family "Iosevka" :size 13 :weight 'regular))
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
@@ -42,7 +46,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-(setq projectile-project-search-path '(("~/prgm/" . 2)))
+(setq projectile-project-search-path '(("~/prgm/" . 2) ("~/prgm/godot/project/" . 2)))
 
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
 
@@ -70,6 +74,10 @@
 
 ;; Disable evil-snipe.
 (remove-hook 'doom-first-input-hook #'evil-snipe-mode)
+
+;; Disable exit prompt when running in text-mode.
+(if (not (display-graphic-p))
+    (setq confirm-kill-emacs nil))
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
@@ -110,8 +118,8 @@
 ;;   (setq rustic-format-trigger 'on-save))
 
 (after! lsp-ui
-  (setq lsp-ui-sideline-show-diagnostics t
-        lsp-ui-sideline-show-code-actions t
+  (setq lsp-ui-sideline-show-diagnostics nil
+        lsp-ui-sideline-show-code-actions nil
         lsp-ui-sideline-diagnostic-max-lines 8))
 
 (map! :n "s" 'avy-goto-char-2)
@@ -131,3 +139,9 @@
 (map! :leader
       :desc "M-x"
       "SPC" #'execute-extended-command)
+
+(map! :after magit
+      :map magit-mode-map
+      :n "q" (lambda () (interactive) (if berry/magit-terminal-buffer-p
+                                          (delete-frame)
+                                        (quit-window))))
